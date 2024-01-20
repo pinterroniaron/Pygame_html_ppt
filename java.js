11f9e7d6086f4d0e593179c12c8e78b4957d94a1
Aron717 <pinterroniaron@gmail.com>
2024-01-20 19:48:32 +0100

    Index

---
+++ java.js
@@ -0,0 +1,45 @@
+gsap.to(".box1", {
+    scrollTrigger: {
+        trigger: ".box1",
+        toggleActions: "restart pause resume reset",
+    },
+    x: 0,
+    duration: 2, 
+    rotation: 0
+
+})
+
+gsap.to(".box2", {
+    scrollTrigger: {
+        trigger: ".box2",
+        toggleActions: "restart pause resume reset"
+    },
+    x: 0,
+    duration: 2, 
+    rotation: 0
+
+})
+
+gsap.to(".box3", {
+    scrollTrigger: {
+        trigger: ".box3",
+        toggleActions: "restart pause resume reset"
+    },
+    x: 0,
+    duration: 2, 
+    rotation: 0
+
+})
+
+gsap.to(".pygame", {
+    scrollTrigger: {
+        trigger: ".pygame",
+        toggleActions: "restart pause resume reset"
+    },
+    x: 0,
+    duration: 2, 
+    rotation: 360
+
+})
+
+
