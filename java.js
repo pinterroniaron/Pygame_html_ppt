e31c940e991798954341863ca51f1644ed971f55
Aron717 <pinterroniaron@gmail.com>
2024-01-21 15:24:55 +0100

    animations2.0

--- java.js
+++ java.js
@@ -50,13 +50,78 @@ gsap.to(".pygame", {
 
 gsap.to(".lecke1", {
     scrollTrigger: {
-        trigger: ".pygame",
+        trigger: ".fadein1",
         toggleActions: "restart pause resume reset"
     },
-    x: 0,
+    opacity: 2,
     y: 0,
-    duration: 2, 
-    rotation: 0,
-    delay: 2
+    duration: 3, 
+    delay: 1
 
+})
+
+gsap.from('.fadein1', { 
+    scrollTrigger:{
+        trigger: ".pygame",
+        toggleActions: "restart pause resume reset",
+    },
+    opacity: 0,
+    y: 20, duration: 1, 
+    delay: 3 
+});
+gsap.from('.fadein2', { 
+    scrollTrigger:{
+        trigger: ".fadein2",
+        toggleActions: "restart pause resume reset",
+    },
+    opacity: 0,
+    y: 20, duration: 1, 
+    delay: 1 
+});
+
+gsap.from('.code', { 
+    scrollTrigger:{
+        trigger: ".code",
+        toggleActions: "restart pause resume reset",
+        start: "top center"
+    },
+    opacity: 0,
+    x: 40, duration: 1.5, 
+    delay: 4,
+});
+
+gsap.from('.py', { 
+    scrollTrigger:{
+        trigger: ".py",
+        toggleActions: "restart pause resume reset",
+        start: "top center"
+    },
+    opacity: 0,
+    x: 40, duration: 1.5, 
+    delay: 4,
+});
+
+gsap.from('.py', { 
+    scrollTrigger:{
+        trigger: ".py",
+        toggleActions: "restart pause resume reset",
+        start: "top center",
+        scrub: 0.5,
+        pin: true,
+        start: "top center",
+        end: "bottom -1000px"
+    },
+    y: "60%", 
+    rotation: 0,
+    delay: 5,
+});
+
+
+
+ScrollTrigger.create({
+    trigger: "#small",
+    start: "top top",
+    end: "+=100%",
+    // pinSpacing: false,
+    pin: true
 })
