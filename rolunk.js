1889b226700ef0a1e1815915629e2a12ba3b9c13
Aron717 <pinterroniaron@gmail.com>
2024-01-29 20:30:56 +0100

    elerhetosegek

--- rolunk.js
+++ rolunk.js
@@ -1,256 +1,52 @@
-console.clear();
+( function($) {
 
-const { gsap, imagesLoaded } = window;
+    $(document).ready(function() {
 
-const buttons = {
-    prev: document.querySelector(".btn--left"),
-    next: document.querySelector(".btn--right"),
-};
-const cardsContainerEl = document.querySelector(".cards__wrapper");
-const appBgContainerEl = document.querySelector(".app__bg");
+        var s           = $('.slider'),
+            sWrapper    = s.find('.slider-wrapper'),
+            sItem       = s.find('.slide'),
+            btn         = s.find('.slider-link'),
+            sWidth      = sItem.width(),
+            sCount      = sItem.length,
+            slide_date  = s.find('.slide-date'),
+            slide_title = s.find('.slide-title'),
+            slide_text  = s.find('.slide-text'),
+            slide_more  = s.find('.slide-more'),
+            slide_image = s.find('.slide-image img'),
+            sTotalWidth = sCount * sWidth;
 
-const cardInfosContainerEl = document.querySelector(".info__wrapper");
+        sWrapper.css('width', sTotalWidth);
+        sWrapper.css('width', sTotalWidth);
 
-buttons.next.addEventListener("click", () => swapCards("right"));
+        var clickCount  = 0;
 
-buttons.prev.addEventListener("click", () => swapCards("left"));
+        btn.on('click', function(e) {
+            e.preventDefault();
 
-function swapCards(direction) {
-    const currentCardEl = cardsContainerEl.querySelector(".current--card");
-    const previousCardEl = cardsContainerEl.querySelector(".previous--card");
-    const nextCardEl = cardsContainerEl.querySelector(".next--card");
+            if( $(this).hasClass('next') ) {
 
-    const currentBgImageEl = appBgContainerEl.querySelector(".current--image");
-    const previousBgImageEl = appBgContainerEl.querySelector(".previous--image");
-    const nextBgImageEl = appBgContainerEl.querySelector(".next--image");
+                ( clickCount < ( sCount - 1 ) ) ? clickCount++ : clickCount = 0;
+            } else if ( $(this).hasClass('prev') ) {
 
-    changeInfo(direction);
-    swapCardsClass();
-
-    removeCardEvents(currentCardEl);
-
-    function swapCardsClass() {
-        currentCardEl.classList.remove("current--card");
-        previousCardEl.classList.remove("previous--card");
-        nextCardEl.classList.remove("next--card");
-
-        currentBgImageEl.classList.remove("current--image");
-        previousBgImageEl.classList.remove("previous--image");
-        nextBgImageEl.classList.remove("next--image");
-
-        currentCardEl.style.zIndex = "50";
-        currentBgImageEl.style.zIndex = "-2";
-
-        if (direction === "right") {
-            previousCardEl.style.zIndex = "20";
-            nextCardEl.style.zIndex = "30";
-
-            nextBgImageEl.style.zIndex = "-1";
-
-            currentCardEl.classList.add("previous--card");
-            previousCardEl.classList.add("next--card");
-            nextCardEl.classList.add("current--card");
-
-            currentBgImageEl.classList.add("previous--image");
-            previousBgImageEl.classList.add("next--image");
-            nextBgImageEl.classList.add("current--image");
-        } else if (direction === "left") {
-            previousCardEl.style.zIndex = "30";
-            nextCardEl.style.zIndex = "20";
-
-            previousBgImageEl.style.zIndex = "-1";
-
-            currentCardEl.classList.add("next--card");
-            previousCardEl.classList.add("current--card");
-            nextCardEl.classList.add("previous--card");
-
-            currentBgImageEl.classList.add("next--image");
-            previousBgImageEl.classList.add("current--image");
-            nextBgImageEl.classList.add("previous--image");
-        }
-    }
-}
-
-function changeInfo(direction) {
-    let currentInfoEl = cardInfosContainerEl.querySelector(".current--info");
-    let previousInfoEl = cardInfosContainerEl.querySelector(".previous--info");
-    let nextInfoEl = cardInfosContainerEl.querySelector(".next--info");
-
-    gsap.timeline()
-        .to([buttons.prev, buttons.next], {
-            duration: 0.2,
-            opacity: 0.5,
-            pointerEvents: "none",
-        })
-        .to(
-            currentInfoEl.querySelectorAll(".text"),
-            {
-                duration: 0.4,
-                stagger: 0.1,
-                translateY: "-120px",
-                opacity: 0,
-            },
-            "-="
-        )
-        .call(() => {
-            swapInfosClass(direction);
-        })
-        .call(() => initCardEvents())
-        .fromTo(
-            direction === "right"
-                ? nextInfoEl.querySelectorAll(".text")
-                : previousInfoEl.querySelectorAll(".text"),
-            {
-                opacity: 0,
-                translateY: "40px",
-            },
-            {
-                duration: 0.4,
-                stagger: 0.1,
-                translateY: "0px",
-                opacity: 1,
+                ( clickCount > 0 ) ? clickCount-- : ( clickCount = sCount - 1 );
             }
-        )
-        .to([buttons.prev, buttons.next], {
-            duration: 0.2,
-            opacity: 1,
-            pointerEvents: "all",
+            TweenMax.to(sWrapper, 0.4, {x: '-' + ( sWidth * clickCount ) })
+
+
+            //CONTENT ANIMATIONS
+
+            var fromProperties = {autoAlpha:0, x:'-50', y:'-10'};
+            var toProperties = {autoAlpha:0.8, x:'0', y:'0'};
+
+            TweenLite.fromTo(slide_image, 1, {autoAlpha:0, y:'40'}, {autoAlpha:1, y:'0'});
+            TweenLite.fromTo(slide_date, 0.4, fromProperties, toProperties);
+            TweenLite.fromTo(slide_title, 0.6, fromProperties, toProperties);
+            TweenLite.fromTo(slide_text, 0.8, fromProperties, toProperties);
+            TweenLite.fromTo(slide_more, 1, fromProperties, toProperties);
+
         });
 
-    function swapInfosClass() {
-        currentInfoEl.classList.remove("current--info");
-        previousInfoEl.classList.remove("previous--info");
-        nextInfoEl.classList.remove("next--info");
-
-        if (direction === "right") {
-            currentInfoEl.classList.add("previous--info");
-            nextInfoEl.classList.add("current--info");
-            previousInfoEl.classList.add("next--info");
-        } else if (direction === "left") {
-            currentInfoEl.classList.add("next--info");
-            nextInfoEl.classList.add("previous--info");
-            previousInfoEl.classList.add("current--info");
-        }
-    }
-}
-
-function updateCard(e) {
-    const card = e.currentTarget;
-    const box = card.getBoundingClientRect();
-    const centerPosition = {
-        x: box.left + box.width / 2,
-        y: box.top + box.height / 2,
-    };
-    let angle = Math.atan2(e.pageX - centerPosition.x, 0) * (35 / Math.PI);
-    gsap.set(card, {
-        "--current-card-rotation-offset": `${angle}deg`,
     });
-    const currentInfoEl = cardInfosContainerEl.querySelector(".current--info");
-    gsap.set(currentInfoEl, {
-        rotateY: `${angle}deg`,
-    });
-}
+})(jQuery);
 
-function resetCardTransforms(e) {
-    const card = e.currentTarget;
-    const currentInfoEl = cardInfosContainerEl.querySelector(".current--info");
-    gsap.set(card, {
-        "--current-card-rotation-offset": 0,
-    });
-    gsap.set(currentInfoEl, {
-        rotateY: 0,
-    });
-}
-
-function initCardEvents() {
-    const currentCardEl = cardsContainerEl.querySelector(".current--card");
-    currentCardEl.addEventListener("pointermove", updateCard);
-    currentCardEl.addEventListener("pointerout", (e) => {
-        resetCardTransforms(e);
-    });
-}
-
-initCardEvents();
-
-function removeCardEvents(card) {
-    card.removeEventListener("pointermove", updateCard);
-}
-
-function init() {
-
-    let tl = gsap.timeline();
-
-    tl.to(cardsContainerEl.children, {
-        delay: 0.15,
-        duration: 0.5,
-        stagger: {
-            ease: "power4.inOut",
-            from: "right",
-            amount: 0.1,
-        },
-        "--card-translateY-offset": "0%",
-    })
-        .to(cardInfosContainerEl.querySelector(".current--info").querySelectorAll(".text"), {
-            delay: 0.5,
-            duration: 0.4,
-            stagger: 0.1,
-            opacity: 1,
-            translateY: 0,
-        })
-        .to(
-            [buttons.prev, buttons.next],
-            {
-                duration: 0.4,
-                opacity: 1,
-                pointerEvents: "all",
-            },
-            "-=0.4"
-        );
-}
-
-const waitForImages = () => {
-    const images = [...document.querySelectorAll("img")];
-    const totalImages = images.length;
-    let loadedImages = 0;
-    const loaderEl = document.querySelector(".loader span");
-
-    gsap.set(cardsContainerEl.children, {
-        "--card-translateY-offset": "100vh",
-    });
-    gsap.set(cardInfosContainerEl.querySelector(".current--info").querySelectorAll(".text"), {
-        translateY: "40px",
-        opacity: 0,
-    });
-    gsap.set([buttons.prev, buttons.next], {
-        pointerEvents: "none",
-        opacity: "0",
-    });
-
-    images.forEach((image) => {
-        imagesLoaded(image, (instance) => {
-            if (instance.isComplete) {
-                loadedImages++;
-                let loadProgress = loadedImages / totalImages;
-
-                gsap.to(loaderEl, {
-                    duration: 1,
-                    scaleX: loadProgress,
-                    backgroundColor: `hsl(${loadProgress * 120}, 100%, 50%`,
-                });
-
-                if (totalImages == loadedImages) {
-                    gsap.timeline()
-                        .to(".loading__wrapper", {
-                            duration: 0.8,
-                            opacity: 0,
-                            pointerEvents: "none",
-                        })
-                        .call(() => init());
-                }
-            }
-        });
-    });
-};
-
-waitForImages();
-
+$('.overlay').addClass('overlay-blue');
